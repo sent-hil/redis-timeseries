1a748b8ed6e1969f28425f143273ffb482f04299
Senthil <senthil@koding.com>
2014-11-23 00:47:55 -0800

    add more comments

--- redis_timeseries.go
+++ redis_timeseries.go
@@ -6,6 +6,19 @@ import (
 	"github.com/jinzhu/now"
 )
 
+// Get returns unix timestamps of the times closest to each time argument
+// as specified by the interval.
+//
+// Ex:
+//   newyear := time.Date(2000, time.January, 01, 0, 0, 30, 0, time.UTC)
+//   newyearplus := time.Date(2000, time.January, 01, 0, 16, 0, 0, time.UTC)
+//
+//   results := Get(15*time.Minute, newyear, newyearplus)
+//
+// `newyear` falls between 00:00 and 00:15, so it returns 00:00.
+// `newyearplus` falls between 00:15 and 00:30, so it returns 00:15.
+//
+//  If a time falls on the interval, ie 00:15, it returns 00.15;
 func Get(interval time.Duration, times ...time.Time) []int64 {
 	var results = []int64{}
 
@@ -25,7 +38,8 @@ func Get(interval time.Duration, times ...time.Time) []int64 {
 				break
 			}
 
-			// this means t must be greater than start+interval
+			// this means t must be greater than start+interval, however
+			// we don't know by how much, so we continue with the loop
 			start = start.Add(interval)
 		}
 	}
